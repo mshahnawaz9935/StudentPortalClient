b22b34167a00ac4796185b5427dba8411189e275
mshahnawaz9935 <mshahnawaz9935@gmail.com>
2017-08-14 17:07:58 +0100

    update

--- src/app/jobs/jobs.component.ts
+++ src/app/jobs/jobs.component.ts
@@ -2,6 +2,7 @@ import {  Http , RequestOptions, Headers, Response } from '@angular/http';
 import { Component, OnInit } from '@angular/core';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
 
 @Component({
   selector: 'app-jobs',
@@ -15,7 +16,7 @@ export class JobsComponent implements OnInit {
 
   constructor(private http:Http ) {
 
-    this.http.get('https://student.almanac-learning.com/onenote/aboutme')
+    this.http.get('http://localhost:50406/api/JobsAPI')
               .map((response : Response) => response.json()).subscribe((Serverdata) => {
                 console.log('Data is ' + Serverdata );
                 this.data = Serverdata;
@@ -24,6 +25,27 @@ export class JobsComponent implements OnInit {
    }
 
   ngOnInit() {
+  }
+      
+  body;
+  apply(title, description, id)
+  { 
+        console.log('here');
+        this.body = { jobid : id , studentid: '1' , companyid: '1' , applydate : '13/8/2017' };
+        let bodyString = JSON.stringify(this.body); // Stringify payload
+        let headers      = new Headers({ 'Content-Type': 'application/json' }); // ... Set content type to JSON
+        let options       = new RequestOptions({ headers: headers }); // Create a request option
+
+        this.http.post('http://localhost:50406/api/AppliedJobsAPI', this.body, options) // ...using post request
+                         .map((res:Response) => res.json()) // ...and calling .json() on the response to return data
+                         .catch((error) => Observable.throw(error.json().error || 'Server error'))
+                         .subscribe((Serverdata) => {
+                              console.log('Data is ' + Serverdata );
+                              this.data = Serverdata;
+                        })
+                          //...errors if any
+   
+
   }
 
 }
