add6c4c49a9e4d8ef9f66c1a136b406945fcb15c
DAIRYMASTER\malam <malam@dairymaster.com>
2018-08-02 17:58:34 +0100

    search box

--- src/app/jobs/jobs.component.ts
+++ src/app/jobs/jobs.component.ts
@@ -14,6 +14,7 @@ import 'rxjs/add/operator/catch';
 export class JobsComponent implements OnInit {
 
   data;
+  searchdata;
   companies;
   constructor(private http:Http , private DataService : DataService  ) {
 
@@ -66,6 +67,7 @@ export class JobsComponent implements OnInit {
 
 
   ngOnInit() {
+    this.searchdata=['Delhi','Kerala','Tamil Nadu','Banglore','Uttar Pradesh','Noida','Haryana','Thrissur'];
   }
       
   body;
